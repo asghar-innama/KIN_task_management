ba7a9394d390eaaaf7a6641fc40b63e12372008a
Innama Asghar <111514134+asghar-innama@users.noreply.github.com>
2025-04-30 17:14:00 +0530

    files added

---
+++ login.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+session_start();
+include('./db_connect.php');
+  ob_start();
+  // if(!isset($_SESSION['system'])){
+
+    $system = $conn->query("SELECT * FROM system_settings")->fetch_array();
+    foreach($system as $k => $v){
+      $_SESSION['system'][$k] = $v;
+    }
+  // }
+  ob_end_flush();
+?>
+<?php 
+  if(isset($_SESSION['login_id']))
+  header("location:index.php?page=home");
+?>
+<?php include 'header.php' ?>
+
+<style>
+  body{
+  margin:0;
+  color: whitesmoke;
+  background: lemonchiffon;
+  font:500 16px/18px 'georgia',serif;
+}
+*,:after,:before{box-sizing:border-box}
+.clearfix:after,.clearfix:before{content:'';display:table}
+.clearfix:after{clear:both;display:block}
+a{color:inherit;text-decoration:none}
+
+.login-wrap {
+  width: 100%;
+  margin: auto;
+  max-width: 500px;
+  min-height: 512px;
+  position: relative;
+  background: url(https://cdn.elearningindustry.com/wp-content/uploads/2020/07/how-to-make-project-management-successful.jpg) no-repeat center;
+  background-size: cover;
+  background-position: center -10px; /* Move the background image down 50px */
+  box-shadow: 0 12px 15px 0 rgba(0,0,0,.24),0 17px 50px 0 rgba(0,0,0,.19);
+}
+
+.login-html{
+  width:100%;
+  height:100%;
+  position:absolute;
+  padding:65px 70px 50px 70px;
+  background:rgba(10,10,160,.7);
+}
+.login-html .sign-in-htm,
+.login-html .sign-up-htm{
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  position:absolute;
+  transform:rotateY(180deg);
+  backface-visibility:hidden;
+  transition:all .4s linear;
+}
+.login-html .sign-in,
+.login-html .sign-up,
+.login-form .group .check{
+  display:none;
+}
+
+.login-html .tab{
+  font-size:19px;
+  margin-right:15px;
+  padding-bottom:10px;
+  margin:0 50px 10px 0;
+  display:inline-block;
+  border-bottom:3px solid transparent;
+}
+.login-html .sign-in:checked + .tab,
+.login-html .sign-up:checked + .tab{
+  color:#fff;
+  border-color:white;
+}
+.login-form{
+  min-height:325px;
+  position:relative;
+  perspective:1000px;
+  transform-style:preserve-3d;
+}
+.login-form .group{
+  margin-bottom:16px;
+}
+.login-form .group .label,
+.login-form .group .input,
+.login-form .group .button{
+  width:100%;
+  color:#fff;
+}
+.login-form .group .input,
+.login-form .group .button{
+  border:none;
+  padding:10px 20px;
+  border-radius:30px;
+  background:rgba(255,255,255,.1);
+}
+.login-form .group input[data-type="password"]{
+  text-security:circle;
+  -webkit-text-security:circle;
+}
+.login-form .group .label{
+  color:white;
+  font-size:15px;
+  font-weight: normal;
+}
+.login-form .group .button {
+background: white;
+color: #000080;
+font-weight: bold;
+}
+.login-form .group label .icon{
+  width:15px;
+  height:15px;
+  border-radius:2px;
+  position:relative;
+  display:inline-block;
+  background:rgba(255,255,255,.1);
+}
+.login-form .group label .icon:before,
+.login-form .group label .icon:after{
+  content:'';
+  width:10px;
+  height:2px;
+  background:#fff;
+  position:absolute;
+  transition:all .2s ease-in-out 0s;
+}
+.login-form .group label .icon:before{
+  left:3px;
+  width:5px;
+  bottom:6px;
+  transform:scale(0) rotate(0);
+}
+.login-form .group label .icon:after{
+  top:6px;
+  right:0;
+  transform:scale(0) rotate(0);
+}
+.login-form .group .check:checked + label{
+  color:#fff;
+}
+.login-form .group .check:checked + label .icon{
+  background:#1161ee;
+}
+.login-form .group .check:checked + label .icon:before{
+  transform:scale(1) rotate(45deg);
+}
+.login-form .group .check:checked + label .icon:after{
+  transform:scale(1) rotate(-45deg);
+}
+.login-html .sign-in:checked + .tab + .sign-up + .tab + .login-form .sign-in-htm{
+  transform:rotate(0);
+}
+.login-html .sign-up:checked + .tab + .login-form .sign-up-htm{
+  transform:rotate(0);
+}
+
+.hr{
+  height:3px;
+  margin:50px 0 50px 0;
+  background:rgba(255,255,255,.3);
+}
+.foot-lnk{
+  text-align:center;
+}
+.login-form .group .button {
+  background: white;
+  color: #000080;
+  font-weight: bold;
+  transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out, transform 0.2s ease-in-out;
+}
+
+.login-form .group .button:hover {
+  background-color: #000080;
+  color: white;
+  transform: scale(1.1);
+}
+.image-container {
+  background-color: white;
+}
+
+.image-container img {
+  width: 50%; 
+}
+
+
+
+</style>
+<body>
+<div class="login-wrap">
+<div class="image-container">
+<img src="assets/uploads/logo.png" alt="logo" style="background-color: white;">
+</div>
+  <form action="" id="login-form">
+      <div class="login-html">
+        <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
+        <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+        <div class="login-form">
+          <div class="sign-in-htm">
+            <div class="group">
+              <label for="user" class="label">Username</label>
+              <input type="email" class="form-control" name="email" required="" placeholder="Email">
+            </div>
+            <div class="group">
+              <label for="pass" class="label">Password</label>
+              <input type="password" class="form-control" name="password" required="" placeholder="Password">
+            </div>
+            <div class="group">
+            <input type="checkbox" id="remember">
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+            <div class="group">
+              <button type="submit" class="button">Sign In</button>
+            </div>
+            <div class="hr"></div>
+            <div class="foot-lnk">
+              <a href="#forgot">Forgot Password?</a>
+            </div>
+          </div>
+          <div class="sign-up-htm">
+            <div class="group">
+              <label for="user" class="label">Username</label>
+              <input id="user" type="text" class="input">
+            </div>
+            <div class="group">
+              <label for="pass" class="label">Password</label>
+              <input id="pass" type="password" class="input" data-type="password">
+            </div>
+            <div class="group">
+              <label for="pass" class="label">Repeat Password</label>
+              <input id="pass" type="password" class="input" data-type="password">
+            </div>
+            <div class="group">
+              <label for="pass" class="label">Email Address</label>
+              <input id="pass" type="text" class="input">
+            </div>
+            <div class="group">
+              <input type="submit" class="button" value="Sign Up">
+            </div>
+            <div class="hr"></div>
+            <div class="foot-lnk">
+              <label for="tab-1">Already Member?</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+</div>
+<script>
+  $(document).ready(function(){
+    $('#login-form').submit(function(e){
+    e.preventDefault()
+    start_load()
+    if($(this).find('.alert-danger').length > 0 )
+      $(this).find('.alert-danger').remove();
+    $.ajax({
+      url:'ajax.php?action=login',
+      method:'POST',
+      data:$(this).serialize(),
+      error:err=>{
+        console.log(err)
+        end_load();
+
+      },
+      success:function(resp){
+        if(resp == 1){
+          location.href ='index.php?page=home';
+        }else{
+          $('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+          end_load();
+        }
+      }
+    })
+  })
+  })
+</script>
+<?php include 'footer.php' ?>
+
+</body>
+</html>
